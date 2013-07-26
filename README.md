1a3638da3c33dd1410ae438f48ca9a333f899d21
Naresh Bhatia <naresh.archfirst@gmail.com>
2013-07-26 17:31:45 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-sports-team-tracker
-===================
+# Sports Team Tracker
 
-A sample application to show how various backbone related frameworks can be combined to create a powerful single-page application.
+This is a sample application to show how various backbone related frameworks can be combined to create a powerful single-page application. The application itself tracks information related to sports teams, e.g. team roster, game schedules etc. We use the following frameworks and technogies to implement the tracker:
+
+* Node.js
+* jQuery
+* [Backbone.js](http://backbonejs.org/)
+* [RequireJS](http://requirejs.org/)
+* [Keel](https://github.com/archfirst/keel) - a light framework around Backbone.js to encourage best practices and conventions
+* [Handlebars](http://handlebarsjs.com/) - a logic-less web template system
+* [Sass](http://sass-lang.com/) / [Compass](http://compass-style.org/): CSS productivity frameworks
+* [Backbone-relational.js] (http://backbonerelational.org/): provides complex relations between Backbone models
+* [Backbone.Validation](http://thedersen.com/projects/backbone-validation/): validates models and form input
+* [backbone.stickit](http://nytimes.github.io/backbone.stickit/): binds Model attributes to View elements provoding two-way binding
+
+## Build Instructions
+
+### Install Build Tools
+* Install [Node.js](http://nodejs.org/).
+* Install [Grunt](https://github.com/gruntjs/grunt/wiki/Getting-started).
+
+    $ npm install -g grunt-cli
+* Install [Ruby](http://rubyinstaller.org/downloads/). (required for compass/sass)
+* Install [Compass](http://compass-style.org/install/) (used for CSS authoring).
+
+    $ gem install compass
+
+### Build Sports Team Tracker
+* Download the sports-team-tracker repository as a zip file or fork it if you plan to make contributions.
+* Open a command shell and change your directory to your local Sports Team Tracker repository.
+* Install Grunt plugins:
+
+    $ npm install
+* Execute the following command to build Sports Team Tracker:
+
+    $ grunt
+* Start the server using the following command:
+
+    $ node server/server.js
+* Start Sports Team Tracker by pointing your browser to [http://localhost:8080](http://localhost:8080). You should see the Home page.
---
+++ client/js/app/domain/Repository.js
@@ -0,0 +1,176 @@
+﻿/**
+ * Copyright 2013 Archfirst
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * app/domain/Repository
+ *
+ * This is a singleton object that maintains the domain layer of the application.
+ * Domain objects in this layer generally live beyond the life of views in the
+ * presentation layer. When views are created, they are generally connected to
+ * domain objects that are already present in this repository.
+ *
+ * @author Naresh Bhatia
+ */
+
+/*jshint devel:true */
+
+define(
+    [
+        'backbone',
+        'relational'
+    ],
+    function(Backbone) {
+        'use strict';
+
+        // Define Person
+        var Person = Backbone.RelationalModel.extend({
+        });
+
+        var PersonCollection = Backbone.Collection.extend({
+            model: Person
+        });
+
+        // Define Team
+        var Team = Backbone.RelationalModel.extend({
+            relations: [
+                {
+                    type: Backbone.HasOne,
+                    key: 'coach',
+                    relatedModel: Person,
+                    includeInJSON: Backbone.Model.prototype.idAttribute
+                },
+                {
+                    type: Backbone.HasMany,
+                    key: 'players',
+                    relatedModel: Person,
+                    includeInJSON: Backbone.Model.prototype.idAttribute,
+                    collectionType: PersonCollection
+                },
+                {
+                    type: Backbone.HasMany,
+                    key: 'games',
+                    relatedModel: 'Game',
+                    includeInJSON: Backbone.Model.prototype.idAttribute,
+                    collectionType: 'GameCollection'
+                }
+            ]
+        });
+
+        var TeamCollection = Backbone.Collection.extend({
+            model: Team
+        });
+
+        // Define Game
+        var Game = Backbone.RelationalModel.extend({
+            relations: [
+                {
+                    type: Backbone.HasMany,
+                    key: 'teams',
+                    relatedModel: Team,
+                    includeInJSON: Backbone.Model.prototype.idAttribute,
+                    collectionType: TeamCollection
+                }
+            ]
+        });
+
+        var GameCollection = Backbone.Collection.extend({
+            model: Game
+        });
+
+
+        // ********************************************************************
+        // Create the Patriots team
+        // ********************************************************************
+
+        // Define people
+        var bill = new Person({
+            firstName: 'Bill',
+            lastName: 'Belichick'
+        });
+
+        var tom = new Person({
+            firstName: 'Tom',
+            lastName: 'Brady'
+        });
+
+        var tim = new Person({
+            firstName: 'Tim',
+            lastName: 'Tebow'
+        });
+
+        // Define the team
+        var patriots = new Team({
+            name: 'Patriots',
+            coach: bill,
+            players: [tom, tim]
+        });
+
+        // ********************************************************************
+        // Create the Jets team
+        // ********************************************************************
+
+        // Define people
+        var rex = new Person({
+            firstName: 'Rex',
+            lastName: 'Ryan'
+        });
+
+        var greg = new Person({
+            firstName: 'Greg',
+            lastName: 'McElroy'
+        });
+
+        var mark = new Person({
+            firstName: 'Mark',
+            lastName: 'Sanchez'
+        });
+
+        // Define the team
+        var nyjets = new Team({
+            name: 'New York Jets',
+            coach: rex,
+            players: [greg, mark]
+        });
+
+        // ********************************************************************
+        // Create a game
+        // ********************************************************************
+        var game1 = new Game({
+            date: new Date(),
+            teams: [patriots, nyjets]
+        });
+
+        var people = new PersonCollection([bill, tom, tim, rex, greg, mark]);
+        var teams = new TeamCollection([patriots, nyjets]);
+        var games = new GameCollection([game1]);
+
+        var _repository = {
+            getPeople: function() {
+                return people;
+            },
+
+            getTeams: function() {
+                return teams;
+            },
+
+            getGames: function() {
+                return games;
+            }
+        };
+
+        return _repository;
+    }
+);
